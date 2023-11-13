9551440e273df5f64e6c2aa92e7e88682a342baf
guptasonu14 <sguptabudha@gmail.com>
2023-11-13 15:26:41 +0530

    header add

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from "../../assets/logo.png";
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="logo-container">
+          {/* Your circle logo goes here */}
+          <img src={Logo} alt="Logo" className="logo" />
+        </div>
+        <div className="separator">|</div>
+        
+        <div className="text">STORIES</div>
+      </div>
+      <div className="header-right">
+        <button className="course-button">Course</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
